c34490f7694ed1aea887521dc270040ef17496cd
Balancer <balancer@balancer.ru>
2015-05-13 02:56:03 +0300

    debug

--- Plugins.php
+++ Plugins.php
@@ -8,12 +8,20 @@ class Plugins
 
 	public static function register($class_map)
 	{
-		foreach($class_map as $group => $classes)
-			\B2\Composer\Cache::appendData('plugins/classes', $classes);
+		echo "\n"; var_dump($class_map); echo "\n";
+
+		\B2\Composer\Cache::appendData('plugins/classes', $class_map);
 
 		\B2\Composer\Cache::addAutoload('plugins/classes', "B2\\Plugins::\$plugins = "
 			.var_export(\B2\Composer\Cache::getData('plugins/classes'), true)
 			.";\n"
 		);
+	}
+
+	public static function classes($group)
+	{
+		require_once COMPOSER_ROOT.'/bors/autoload.php';
+
+		return @self::$plugins[$group];
 	}
 }
