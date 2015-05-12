3960d18d95dfa403750acf1ec23a4ab1e605bb22
Balancer <balancer@balancer.ru>
2015-05-13 02:47:49 +0300

    fix

--- Plugins.php
+++ Plugins.php
@@ -8,15 +8,14 @@ class Plugins
 
 	public static function register($class_map)
 	{
-		foreach($class_map as $group => $classes)
-		{
-			foreach($classes as $class)
-				\B2\Composer\Cache::appendData('plugins/classes/'.$group, $class);
-		}
+		echo "\nregister".print_r($class_map, true)."\n";
 
-		\B2\Composer\Cache::addAutoload('plugins', "B2\\Plugins::plugins = "
+		foreach($class_map as $group => $classes)
+			\B2\Composer\Cache::appendData('plugins/classes', $classes);
+
+		\B2\Composer\Cache::addAutoload('plugins/classes', "B2\\Plugins::plugins = "
 			.var_export(\B2\Composer\Cache::getData('plugins/classes'), true)
-			.";\n\n"
+			.";\n"
 		);
 	}
 }
